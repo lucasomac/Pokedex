5bf3883f5db84a039b0a3be1d077e2badf93bb2b
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2024-10-07 16:14:27 -0300

    test(unitTest): Implemented unit tests for pokedex call and inserted dependencies for tests in android

--- app/src/test/java/br/com/lucolimac/pokedex/data/repository/PokedexRepositoryImplTest.kt
+++ app/src/test/java/br/com/lucolimac/pokedex/data/repository/PokedexRepositoryImplTest.kt
@@ -1,3 +1,41 @@
 package br.com.lucolimac.pokedex.data.repository
 
-class PokedexRepositoryImplTest
+import br.com.lucolimac.pokedex.data.model.response.PokemonListResponse
+import br.com.lucolimac.pokedex.data.source.PokedexDataSource
+import br.com.lucolimac.pokedex.domain.repository.PokedexRepository
+import br.com.lucolimac.pokedex.domain.util.Result
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.hamcrest.MatcherAssert
+import org.hamcrest.core.IsInstanceOf
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import retrofit2.Response
+
+class PokedexRepositoryImplTest {
+    private lateinit var dataSource: PokedexDataSource
+    private lateinit var repository: PokedexRepository
+
+    @Before
+    fun setUp() {
+        dataSource = mockk()
+        repository = PokedexRepositoryImpl(dataSource)
+    }
+
+    @Test
+    fun `Should be return success when call get pokedex`() {
+        runTest {
+            val pokedexResponse = Response.success(PokemonListResponse(0, "", "", listOf()))
+            coEvery { dataSource.getPokemonList(10, 10) } returns pokedexResponse
+
+            val result = repository.getPokemonList(10, 10)
+            val pokedex = Result.Success(PokemonListResponse(0, "", "", listOf()).toEntity())
+            MatcherAssert.assertThat(
+                result, IsInstanceOf(Result.Success::class.java)
+            )
+            assertEquals(pokedex.data, (result as Result.Success).data)
+        }
+    }
+}
